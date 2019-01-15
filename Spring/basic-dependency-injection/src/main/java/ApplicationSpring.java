8926024b71aa94fd70c522379ebfce1ca6fc6f3e
Kris <krzysztof.kozubek135@gmail.com>
2019-01-15 19:57:36 +0100

    basic-dependency-injection description

--- Spring/basic-dependency-injection/src/main/java/ApplicationSpring.java
+++ Spring/basic-dependency-injection/src/main/java/ApplicationSpring.java
@@ -26,6 +26,7 @@ public class ApplicationSpring {
     }
 
     private static void factoryMethod(ApplicationContext context) {
+        System.out.println("Factory Method - call static method");
         DateFormat format = context.getBean("formatter", DateFormat.class);
         Calendar calendar = context.getBean("calendar", Calendar.class);
         System.out.println(format.format(calendar.getTime()));
@@ -40,38 +41,49 @@ public class ApplicationSpring {
         System.out.println(someBean);
         someBean = context.getBean("someBeanPIC2", SomeBean.class);
         System.out.println(someBean);
+        System.out.println();
     }
 
     private static void log4j(ApplicationContext context) {
+        System.out.println("log4j - lib to display logs");
         String someString = context.getBean("someString", String.class);
         System.out.println(someString);
+        System.out.println();
     }
 
     private static void innerBean(ApplicationContext context) {
+        System.out.println("Inner Bean - create bean through constructor and properties");
         User user = context.getBean("userWithInnerBean", User.class);
         System.out.println(user);
+        System.out.println();
     }
 
     private static void collectionBean(ApplicationContext context) {
+        System.out.println("Collection - inject collections (array, list, set, map, properties)");
         User user = context.getBean("collectionSomeBean", User.class);
         System.out.println(user);
 
         user = context.getBean("mapSomeBean", User.class);
         System.out.println(user);
+        System.out.println();
     }
 
     private static void autowired(ApplicationContext context) {
+        System.out.println("Autowired - by_name, by_type, by_constructor");
         tmp.SomeBean someBean = context.getBean("someInjectedBean", tmp.SomeBean.class);
         System.out.println(someBean);
 
         tmp.Repositorys repositorys = context.getBean("repositoryBean", tmp.Repositorys.class);
         System.out.println(repositorys);
+        System.out.println();
     }
 
     public static void initBeforeBeanHasBeenCreated(ApplicationContext context) {
+        System.out.println("Init method - init method before bean has been created");
         tmp.InitMethodBeforeCreateBean initMethodBeforeCreateBean = context.getBean("initMethodBeforeCreateBean", tmp.InitMethodBeforeCreateBean.class);
         System.out.println(initMethodBeforeCreateBean);
         initMethodBeforeCreateBean = context.getBean("initMethodUsingInterface", tmp.InitMethodBeforeCreateBean.class);
         System.out.println(initMethodBeforeCreateBean);
+        System.out.println();
     }
 }
