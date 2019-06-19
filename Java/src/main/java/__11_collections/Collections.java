f763f27b2b440c6f0c80398253269bb21b6ae2bc
Kris <krzysztof.kozubek135@gmail.com>
2019-06-19 16:57:35 +0200

    Java- OOP # part 11

--- Java/src/main/java/__11_collections/Collections.java
+++ Java/src/main/java/__11_collections/Collections.java
@@ -38,4 +38,36 @@ package __11_collections;
 *       NN - not null
 * */
 
-public class Collections {}
+import java.util.*;
+
+public class Collections {
+
+    private void list() {
+        List<Object> list = new ArrayList<>();
+        ArrayList<Object> arrayList = new ArrayList<>();
+        LinkedList<Object> linkedList = new LinkedList<>();
+        Vector<Object> vector = new Vector<>();
+        Stack<Object> stack = new Stack<>();
+
+        linkedList.getFirst();
+        linkedList.getLast();
+        linkedList.offerFirst(new Object()); // add on start list ele
+        linkedList.offerLast(new Object()); // add on end list ele
+        linkedList.peekFirst(); // Retrieves, but does not remove
+        linkedList.peekLast(); // Retrieves, but does not remove
+        linkedList.poll(); // Retrieves and removes the head
+        linkedList.pop(); // Pops an element from the stack represented by this list
+    }
+
+    private void set() {
+        HashSet<Object> hashSet = new HashSet<>();
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+
+        TreeSet<Object> treeSet = new TreeSet<>();
+        treeSet.ceiling(new Object()); // Returns the least element in this set greater than or equal to the given element, or null if there is no such element.
+    }
+
+    private void queue() {
+        PriorityQueue<Object> priorityQueue = new PriorityQueue<>();
+    }
+}
